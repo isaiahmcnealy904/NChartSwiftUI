e2344717b6aac80dd5a7179dc65329a4d8edd679
Isaiah <isaiah_mcnealy_o@lilly.com>
2020-06-08 09:08:39 -0400

    Initial Commit

---
+++ NChart3D-Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NChart3D-Demo
+//
+//  Created by Isaiah McNealy on 6/8/20.
+//  Copyright © 2020 Isaiah McNealy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
